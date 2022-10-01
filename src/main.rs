dbf72142cbcd5f70f4797b276be579c083744017
Rafal Ulko <rafal.ulko@gmail.com>
2022-10-01 09:21:39 +0200

    Add more directories that could contain .desktop files

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,19 @@ use anyhow::Result;
 
 fn main() -> Result<()> {
     let apps_thread = thread::spawn(|| {
-        let mut applications = std::fs::read_dir("/usr/share/applications")?
-            .into_iter()
-            .map(|file| file.unwrap().path())
+        let user_home_app_dir = format!("{}/.local/share/applications", env!("HOME"));
+        let app_dirs = vec![
+            "/usr/share/applications",
+            "/usr/local/share/applications",
+            &user_home_app_dir,
+        ];
+
+        let mut applications = app_dirs
+            .iter()
+            .filter_map(|path| std::fs::read_dir(path).ok())
+            .flatten()
+            .filter_map(|file| file.ok())
+            .map(|file| file.path())
             .filter(|path| path.extension().and_then(|x| x.to_str()) == Some("desktop"))
             .filter_map(|file| model::Application::from_freedesktop_file(file).transpose())
             .collect::<Result<Vec<_>>>()?;
