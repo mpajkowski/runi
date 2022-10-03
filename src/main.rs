c3f6014864e58201a0299076c1b9ae5ce9c3c2e1
Rafal Ulko <rafal.ulko@gmail.com>
2022-10-03 21:11:29 +0200

    Use XDG_DATA_DIRS and XDG_DATA_HOME env variables as reference to find .desktop files

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,13 @@
 pub mod model;
 mod ui;
 
-use std::thread;
+use std::{env, path::PathBuf, str::FromStr, thread};
 
 use anyhow::Result;
 
 fn main() -> Result<()> {
     let apps_thread = thread::spawn(|| {
-        let user_home_app_dir = format!("{}/.local/share/applications", env!("HOME"));
-        let app_dirs = vec![
-            "/usr/share/applications",
-            "/usr/local/share/applications",
-            &user_home_app_dir,
-        ];
+        let app_dirs = apps_dirs()?;
 
         let mut applications = app_dirs
             .iter()
@@ -32,4 +27,28 @@ fn main() -> Result<()> {
     ui::run_ui(apps_thread);
 
     Ok(())
+}
+
+fn apps_dirs() -> Result<Vec<PathBuf>> {
+    let xdg_data_dirs = env::var("XDG_DATA_DIRS")?;
+
+    let mut apps_dirs = xdg_data_dirs
+        .split(":")
+        .map(|d| PathBuf::from_str(d).map_err(|err| err.into()))
+        .collect::<Result<Vec<_>>>()?;
+
+    let xdg_data_home = match env::var("XDG_DATA_HOME") {
+        Ok(p) => PathBuf::from_str(&p)?,
+        Err(_) => {
+            let home_dir = env::var("HOME")?;
+            let mut xdg_data_home = PathBuf::from_str(&home_dir)?;
+            xdg_data_home.push(".local/share");
+            xdg_data_home
+        }
+    };
+
+    apps_dirs.push(xdg_data_home);
+    apps_dirs.iter_mut().for_each(|d| d.push("applications"));
+
+    Ok(apps_dirs)
 }
